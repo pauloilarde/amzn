3d710045c25e375731af5ff8704637a676f2d837
Paulo Ilarde <pauloilarde@gmail.com>
2020-11-18 11:29:59 -0300

    Criando o projeto com react e fazendo layout inicial da página

---
+++ frontend/src/data.js
@@ -0,0 +1,72 @@
+const data = {
+    products: [
+        {
+            _id: 1,
+            name: 'T shirt nike',
+            category: 'Shirt',
+            image: '/img/p1.jpg',
+            price: 120,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 15,
+            description: 'High quality clothes'
+        },
+        {
+            _id: 2,
+            name: 'T shirt adidas',
+            category: 'Shirt',
+            image: '/img/p2.jpg',
+            price: 130,
+            brand: 'Adidas',
+            rating: 4.8,
+            numReviews: 22,
+            description: 'High quality clothes'
+        },
+        {
+            _id: 3,
+            name: 'T shirt lacoste',
+            category: 'Shirt',
+            image: '/img/p3.jpg',
+            price: 150,
+            brand: 'Lacoste',
+            rating: 4.2,
+            numReviews: 35,
+            description: 'High quality clothes'
+        },
+        {
+            _id: 4,
+            name: 'Pant nike',
+            category: 'Pants',
+            image: '/img/p1.jpg',
+            price: 160,
+            brand: 'Nike',
+            rating: 4.8,
+            numReviews: 23,
+            description: 'High quality clothes'
+        },
+        {
+            _id: 5,
+            name: 'Pant adidas',
+            category: 'Shirt',
+            image: '/img/p5.jpg',
+            price: 170,
+            brand: 'Adidas',
+            rating: 4.9,
+            numReviews: 15,
+            description: 'High quality clothes'
+        },
+        {
+            _id: 6,
+            name: 'Pant lacoste',
+            category: 'Shirt',
+            image: '/img/p6.jpg',
+            price: 180,
+            brand: 'Lacoste',
+            rating: 4.7,
+            numReviews: 9,
+            description: 'High quality clothes'
+        }
+    ]
+}
+
+export default data;
